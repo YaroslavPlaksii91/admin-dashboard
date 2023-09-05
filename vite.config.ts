dfae70daee3fff5802b1968c4fea39d6026dbbd2
YaroslavPlaksii91 <plaksii.yaroslav@gmail.com>
2023-09-05 16:48:05 +0300

    remove aliases

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@components': '/src/components',
-      '@layout': '/src/layout',
-      '@pages': '/src/pages',
-      '@redux': '/src/redux',
-      '@assets': '/src/assets',
-      '@routes': '/src/routes',
-      '@services': '/src/services',
-      '@utils': '/src/utils',
-    },
-  },
 });
