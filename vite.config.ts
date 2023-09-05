b725c5fb71b0bef63f5d87946254d5b05942d66c
YaroslavPlaksii91 <plaksii.yaroslav@gmail.com>
2023-09-05 23:30:41 +0300

    add colors and common styles

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
       '@routes': '/src/routes',
       '@services': '/src/services',
       '@utils': '/src/utils',
+      '@styles': '/src/styles',
     },
   },
 });
