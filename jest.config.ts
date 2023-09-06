6f5adc199bab1252dc6cdec7fca9bb3ad80ff089
YaroslavPlaksii91 <plaksii.yaroslav@gmail.com>
2023-09-06 14:51:40 +0300

    update jest settings

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,16 @@
-module.exports = {
+import type { Config } from 'jest';
+
+const config: Config = {
+  verbose: true,
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
+  moduleNameMapper: {
+    '\\.css$': 'jest-css-modules',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
+
+export default config;
