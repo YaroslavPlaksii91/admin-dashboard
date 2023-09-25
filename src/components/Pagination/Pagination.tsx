d82ffc4627908414867fa3176c23088e3bfb2a11
YaroslavPlaksii91 <plaksii.yaroslav@gmail.com>
2023-09-25 14:46:26 +0300

    create pagination component

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,47 @@
+import { useState, MouseEvent, ChangeEvent, FC } from 'react';
+import { TablePagination } from '@mui/material';
+
+import { ROWS_PER_PAGE, ROWS_PER_PAGE_OPTIONS } from './constants';
+import { PaginationProps } from './types';
+
+export const Pagination: FC<PaginationProps> = ({ count, page, setPage }) => {
+  const [rowsPerPage, setRowsPerPage] = useState(ROWS_PER_PAGE);
+
+  const handleChangePage = (
+    _: MouseEvent<HTMLButtonElement> | null,
+    newPage: number,
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <TablePagination
+      component="div"
+      count={count}
+      page={page}
+      onPageChange={handleChangePage}
+      rowsPerPage={rowsPerPage}
+      onRowsPerPageChange={handleChangeRowsPerPage}
+      rowsPerPageOptions={ROWS_PER_PAGE_OPTIONS}
+      sx={{
+        color: 'secondaryTextColor',
+        '& .MuiToolbar-root': {
+          padding: '16px 32px 16px',
+        },
+        '& .MuiSelect-icon': {
+          fill: '#9FA2B4',
+        },
+        '& .MuiSelect-select': {
+          color: 'grayDarkColor',
+        },
+      }}
+    />
+  );
+};
