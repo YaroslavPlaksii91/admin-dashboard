194cb699746e73fe352db19b1358ab4758b88314
YaroslavPlaksii91 <plaksii.yaroslav@gmail.com>
2023-09-25 14:49:33 +0300

    add tickets page

--- src/layout/Tickets/Tickets.tsx
+++ src/layout/Tickets/Tickets.tsx
@@ -1,5 +1,49 @@
 import { FC } from 'react';
+import { Box } from '@mui/material';
+
+import { Pagination } from '@components/Pagination/Pagination';
+import { ActionButtons } from '@components/ActionButtons/ActionButtons';
+
+import { TicketsItem } from './components/TicketsItem/TicketsItem';
+import { Heading } from './components/Heading/Heading';
+import { useTickets } from './useTickets';
 
 export const Tickets: FC = () => {
-  return <div>Tickets</div>;
+  const {
+    getCurrentPageTickets,
+    page,
+    setPage,
+    onAddClick,
+    onFilterClick,
+    onSortClick,
+    ticketsCount,
+  } = useTickets();
+
+  return (
+    <Box
+      sx={{
+        height: '100%',
+        pt: '37px',
+        border: '1px solid',
+        borderColor: 'borderColor',
+        borderRadius: '8px',
+        backgroundColor: 'mainWhiteColor',
+      }}
+    >
+      <ActionButtons
+        addButtonName="Add ticket"
+        onAddClick={onAddClick}
+        onFilterClick={onFilterClick}
+        onSortClick={onSortClick}
+      />
+
+      <Heading />
+
+      {getCurrentPageTickets().map(ticket => (
+        <TicketsItem ticket={ticket} key={ticket.id} />
+      ))}
+
+      <Pagination count={ticketsCount} page={page} setPage={setPage} />
+    </Box>
+  );
 };
