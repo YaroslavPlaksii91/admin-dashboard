e090d509f80c51b8f5611689fd58cdcea51c6270
YaroslavPlaksii91 <plaksii.yaroslav@gmail.com>
2023-09-26 11:04:23 +0300

    fix paths

--- jest.config.ts
+++ jest.config.ts
@@ -12,8 +12,6 @@ const config: Config = {
     '@components(.*)$': '<rootDir>/src/components/$1',
     '@routes(.*)$': '<rootDir>/src/routes/$1',
     '@pages(.*)$': '<rootDir>/src/pages/$1',
-    '@layout(.*)$': '<rootDir>/src/layout/$1',
-    '@redux(.*)$': '<rootDir>/src/redux/$1',
     '@styles(.*)$': '<rootDir>/src/styles/$1',
     '@services(.*)$': '<rootDir>/src/services/$1',
     '@utils(.*)$': '<rootDir>/src/utils/$1',
