b9f83cbdb94865996d8b90edc12146a17b0e1193
YaroslavPlaksii91 <plaksii.yaroslav@gmail.com>
2023-09-27 15:34:19 +0300

    add datepicker

---
+++ src/components/DatePicker/DatePicker.tsx
@@ -0,0 +1,35 @@
+import { FC } from 'react';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateTimePicker, LocalizationProvider } from '@mui/x-date-pickers';
+
+import { DatePickerProps } from './types';
+
+export const DatePicker: FC<DatePickerProps> = ({
+  errors,
+  field,
+  label,
+  name,
+}) => {
+  const errorMessage = errors[name]?.message?.toString();
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateTimePicker
+        label={label}
+        value={field.value || null}
+        inputRef={field.ref}
+        onChange={field.onChange}
+        onAccept={field.onChange}
+        slotProps={{
+          textField: {
+            variant: 'outlined',
+            fullWidth: true,
+            InputLabelProps: { shrink: true },
+            error: !!errors[name],
+            helperText: errorMessage || '',
+          },
+        }}
+      />
+    </LocalizationProvider>
+  );
+};
