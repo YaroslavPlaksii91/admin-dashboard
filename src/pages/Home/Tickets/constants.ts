5ba960698313f1c39587a2f8b212bc5d74887a45
YaroslavPlaksii91 <plaksii.yaroslav@gmail.com>
2023-09-29 18:06:05 +0300

    add sort to tickets

---
+++ src/pages/Home/Tickets/constants.ts
@@ -0,0 +1,22 @@
+export const TICKETS_COLUMNS = Object.freeze([
+  {
+    title: 'Ticket details',
+    name: 'title',
+    size: 4,
+  },
+  {
+    title: 'Customer name',
+    name: 'customer',
+    size: 3,
+  },
+  {
+    title: 'Date',
+    name: 'date',
+    size: 3,
+  },
+  {
+    title: 'Priority',
+    name: 'priority',
+    size: 2,
+  },
+]);
