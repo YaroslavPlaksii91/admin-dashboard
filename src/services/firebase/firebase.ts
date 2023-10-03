904bbee9c8a4bc56af260ec7e521ade027721f85
YaroslavPlaksii91 <plaksii.yaroslav@gmail.com>
2023-10-03 15:12:02 +0300

    move firebase config to env file

--- src/services/firebase/firebase.ts
+++ src/services/firebase/firebase.ts
@@ -8,13 +8,13 @@ import {
 } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDi2Tz4_08rci_ueySrxV74Qb0XwVjUv3c',
-  authDomain: 'admin-dashboard-fe151.firebaseapp.com',
-  projectId: 'admin-dashboard-fe151',
-  storageBucket: 'admin-dashboard-fe151.appspot.com',
-  messagingSenderId: '344566717061',
-  appId: '1:344566717061:web:7ebb607d2d88ceca7527ec',
-  measurementId: 'G-49PBLQE76Y',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
