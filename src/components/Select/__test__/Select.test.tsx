c933b958937a12ef5bc8ab621a62ee034ebd76ee
YaroslavPlaksii91 <plaksii.yaroslav@gmail.com>
2023-10-09 12:38:19 +0300

    add select tests

---
+++ src/components/Select/__test__/Select.test.tsx
@@ -0,0 +1,78 @@
+import { cleanup, render, screen } from '@testing-library/react';
+import {
+  UseFormRegisterReturn,
+  FieldErrors,
+  FieldError,
+} from 'react-hook-form';
+
+import { Select } from '../Select';
+
+describe('Select component', () => {
+  afterEach(cleanup);
+
+  const options = [
+    { label: 'Option 1', value: 'option1' },
+    { label: 'Option 2', value: 'option2' },
+    { label: 'Option 3', value: 'option3' },
+  ];
+  const label = 'Select component';
+  const name = 'select';
+  const register: UseFormRegisterReturn<any> = {
+    onChange: jest.fn(),
+    onBlur: jest.fn(),
+    name: name,
+    ref: null as any,
+  };
+  const errors: FieldErrors = {};
+
+  it('should be rendered', () => {
+    const { asFragment } = render(
+      <Select
+        label={label}
+        name={name}
+        register={register}
+        errors={errors}
+        options={options}
+      />,
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('renders correctly', () => {
+    render(
+      <Select
+        label={label}
+        name={name}
+        register={register}
+        errors={errors}
+        options={options}
+      />,
+    );
+
+    const labelElement = screen.getByLabelText(label);
+    const selectElement = screen.getByRole('button');
+
+    expect(labelElement).toBeInTheDocument();
+    expect(selectElement).toBeInTheDocument();
+  });
+
+  it('displays error and helper text when there is an error', () => {
+    const errors: FieldErrors = {
+      [name]: { message: 'This field is required' } as FieldError,
+    };
+
+    render(
+      <Select
+        label={label}
+        name={name}
+        register={register}
+        errors={errors}
+        options={options}
+      />,
+    );
+
+    const errorElement = screen.getByText('This field is required');
+    expect(errorElement).toBeInTheDocument();
+  });
+});
