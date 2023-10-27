5dfd5426a53ac756762be0fbfb30b2e046f81d51
YaroslavPlaksii91 <plaksii.yaroslav@gmail.com>
2023-10-27 13:18:59 +0300

    change input tests

--- src/utils/testMocks.ts
+++ src/utils/testMocks.ts
@@ -1,10 +1,10 @@
 import { UseFormRegisterReturn } from 'react-hook-form';
 
-export const createRegister = (name: string): UseFormRegisterReturn<any> => {
+export const createRegister = (name: string): UseFormRegisterReturn => {
   return {
     onChange: jest.fn(),
     onBlur: jest.fn(),
     name: name,
-    ref: null as any,
+    ref: jest.mocked,
   };
 };
