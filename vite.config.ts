e1791bd1a6963ed5d29c5654b0243b5401eb0fd3
YaroslavPlaksii91 <plaksii.yaroslav@gmail.com>
2023-11-15 12:52:09 +0200

    add pwa to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,39 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
+const vitePWA = VitePWA({
+  registerType: 'autoUpdate',
+  workbox: {
+    globDirectory: 'dist/',
+    globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+    swDest: 'dist/sw.js',
+    globIgnores: ['**/node_modules/**/*', 'sw.js', 'workbox-*.js'],
+  },
+  outDir: 'dist',
+  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+  manifest: {
+    name: 'Admin Dashboard',
+    short_name: 'Admin Dashboard',
+    description: 'Modern Admin Dashboard for Seamless Management',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
+});
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vitePWA],
   resolve: {
     alias: {
       '@components': '/src/components',
